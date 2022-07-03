5c22b96b38334a0f9925bbd7ebb727b94ff87ed2
Nelson <nelsonlarajr@gmail.com>
2022-07-03 15:37:56 -0300

    alteração no settings.py

--- django1/settings.py
+++ django1/settings.py
@@ -10,10 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-#import django_heroku
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -127,3 +129,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #USADO DURANTE A PRODUÇÃO
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGOUT_REDIRECT_URL = 'index'
+
+django_heroku.settings(locals())
